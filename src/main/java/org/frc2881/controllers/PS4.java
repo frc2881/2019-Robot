19ab8b5df9b892a6294d8da5566c212483290356
Lady Cans <frc2881@gmail.com>
2019-01-19 19:03:29 -0600

    OI should be okay (added PS4 interface, comments, changed button names, added parameters to LiftToHeight and ArmToHeight)

---
+++ src/main/java/org/frc2881/controllers/PS4.java
@@ -0,0 +1,31 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.frc2881.controllers;
+
+/**
+ * Add your docs here.
+ */
+public interface PS4 {
+    int BLUE_X = 1;
+    int RED_CIRCLE = 2;
+    int PINK_SQUARE = 3;
+    int GREEN_TRIANGLE = 4;
+    int LEFT_BUMPER = 5;
+    int RIGHT_BUMPER = 6;
+    int SHARE_BUTTON = 7;
+    int OPTIONS_BUTTON = 8;
+    int LEFT_JOYSTICK_PRESS = 9;
+    int RIGHT_JOYSTICK_PRESS = 10;
+
+    int LEFT_JOYSTICK_HORIZONTAL = 0;
+    int LEFT_JOYSTICK_VERTICAL = 1;
+    int LEFT_TRIGGER = 2;
+    int RIGHT_TRIGGER = 3;
+    int RIGHT_JOYSTICK_HORIZONTAL = 4;
+    int RIGHT_JOYSTICK_VERTICAL = 5;
+}
