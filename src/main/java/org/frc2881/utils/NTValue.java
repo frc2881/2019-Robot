5264970e551cb7bd51d7710f5f5efa529ad491be
Lady Cans <frc2881@gmail.com>
2019-07-06 18:44:29 -0500

    Fixed biggest cargo to accomodate more than two cargo detected. working on target
    STUDENTS: Tian
    MENTORS: Brian

--- src/main/java/org/frc2881/utils/NTValue.java
+++ src/main/java/org/frc2881/utils/NTValue.java
@@ -10,7 +10,14 @@ public class NTValue {
     private static NetworkTable table;
     private static NetworkTableEntry cameraForward;
     private static NetworkTableEntry cargoInfo;
+    private static NetworkTableEntry targetInfo;
     private static double[] cargoInfoArray;
+    private static Double[][] cargo;
+    private static double[] targetInfoArray;
+    private static double[] targetInfo1;
+    private static double[] targetInfo2;
+    private static double[] targetInfo3;
+    private static double[] targetInfo4;
     private static double[] defaultValue = new double[0];
     private Boolean first;
 
@@ -19,6 +26,7 @@ public class NTValue {
         table = ntinst.getTable("RPi");
         cameraForward = table.getEntry("cameraForward");
         cargoInfo = table.getEntry("cargoInfo");
+        targetInfo = table.getEntry("targetInfo");
     }
 
     public static void setCameraForward(boolean forward) {
@@ -29,56 +37,60 @@ public class NTValue {
         return cameraForward.getBoolean(true);
     } 
 
-    public static Boolean biggerCargoFirst() {
+    public static int biggestCargoIndex() {
         cargoInfoArray = cargoInfo.getDoubleArray(defaultValue);
+        int totalCargo = cargoInfoArray.length / 3;
+        cargo = new Double[totalCargo][3];
+        int biggestCargo = 0;
 
-        if (cargoInfoArray.length == 0) {
-            return true;
+        for (int i = 0; i < totalCargo; i++) {
+            cargo[i][0] = cargoInfoArray[i * 3];//x
+            cargo[i][1] = cargoInfoArray[i * 3 + 1];//y
+            cargo[i][2] = cargoInfoArray[i * 3 + 2];//size
         }
-        else if (cargoInfoArray.length > 3 && cargoInfoArray[2] < cargoInfoArray[5]) {
-            return false;
+
+        for (int i = 0; i < totalCargo; i++) {
+            if (cargo[i][3] > cargo[biggestCargo][3]){  
+                biggestCargo = i;
+            }  
         }
-        else {
-            return true;
+
+        return biggestCargo;
+
+    }
+
+    private static void groupLines() {
+        targetInfoArray = targetInfo.getDoubleArray(defaultValue);
+
+        Double[][] line = new Double[targetInfoArray.length / 4][4];
+        Double x1;
+        Double y1;
+        Double x2;
+        Double y2;
+        Double angle;
+        
+        for (int i = 0; i < targetInfoArray.length / 4; i++) {
+            x1 = line[i][0] = cargoInfoArray[i * 4];//x1
+            y1 = line[i][1] = cargoInfoArray[i * 4 + 1];//y1
+            x2 = line[i][2] = cargoInfoArray[i * 4 + 2];//x2
+            y2 = line[i][3] = cargoInfoArray[i * 4 + 3];//y2
+            angle = line[i][4] = Math.atan((x2-x1)/(y2-y1));//angle
         }
+        
     }
 
     public static double getCargoX() {
         //find one with largest radius first
-        if (biggerCargoFirst()){
-            if (cargoInfoArray.length == 0) {
-                return -1;
-            }
-            return cargoInfoArray[0];
-        }
-        else {
-            return cargoInfoArray[3];
-        }
+        return cargo[biggestCargoIndex()][0];
     }
 
     public static double getCargoY() {
         //find one with largest radius first
-        if (biggerCargoFirst()){
-            if (cargoInfoArray.length == 0) {
-                return -1;
-            }
-            return cargoInfoArray[1];
-        }
-        else {
-            return cargoInfoArray[4];
-        }
+        return cargo[biggestCargoIndex()][1];
     }
 
     public static double getCargoR() {
         //find one with largest radius first
-        if (biggerCargoFirst()){
-            if (cargoInfoArray.length == 0) {
-                return -1;
-            }
-            return cargoInfoArray[2];
-        }
-        else {
-            return cargoInfoArray[5];
-        }
+        return cargo[biggestCargoIndex()][2];
     }
 }
